9340faaf594bb2eeba6adb86a19865ccd5a0fb21
vasya <vasya1010101010@gmail.com>
2022-09-27 17:20:00 +0300

    Refactoring main.py

---
+++ homework_01/main.py
@@ -0,0 +1,52 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*num):
+    return [num ** 2 for num in num]
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def is_prime(p_int: int) -> bool:
+    flag = 0
+    for i in range(2, (p_int // 2) + 1):
+        if p_int % i == 0:
+            flag = 1
+            break
+
+    if flag == 0:
+        return True
+    else:
+        return False
+
+
+def filter_numbers(p_list: list, p_type_item: str) -> list:
+    if p_type_item == ODD:
+        return list(filter(lambda x: x % 2 != 0, p_list))
+    elif p_type_item == EVEN:
+        return list(filter(lambda x: x % 2 == 0, p_list))
+    elif p_type_item == PRIME:
+        return list(filter(is_prime, p_list))
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
